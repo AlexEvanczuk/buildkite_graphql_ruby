1ac5498859cd25ca40d8b716701b928a739848f3
Alex Evanczuk <alexevanczuk@gmail.com>
2018-12-10 11:18:15 -0800

    Add query builder

---
+++ lib/buildkite_graphql_ruby/query_builder.rb
@@ -0,0 +1,56 @@
+module BuildkiteGraphqlRuby
+  class QueryBuilder
+    def branch_status(branch:)
+      query = <<~EOS
+        {
+          viewer {
+            user {
+              name
+              builds(branch:"#{branch}") {
+                count
+                edges {
+                  node{
+                    branch
+                    state
+                    url
+                    uuid
+                    scheduledAt
+                    startedAt
+                    finishedAt
+                    pullRequest {
+                      id
+                    }
+                    jobs(first: 100) {
+                      edges {
+                        node {
+                          \.\.\. on JobTypeCommand {
+                            agent {
+                              id
+                            }
+                            artifacts(first: 100) {
+                              edges {
+                                node {
+                                  id
+                                  path
+                                  
+                                  state
+                                  downloadURL
+                                }
+                              }
+                            }
+                            command
+                            url
+                          }
+                        }
+                      }
+                    }
+                  } 
+                }
+              }
+            }
+          }
+        }
+        EOS
+    end
+  end
+end
