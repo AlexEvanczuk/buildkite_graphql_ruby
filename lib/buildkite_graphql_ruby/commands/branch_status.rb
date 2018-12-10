e2025a3261989d9ae00d91c642aa67a7cdf14225
Alex Evanczuk <alexevanczuk@gmail.com>
2018-12-10 11:19:06 -0800

    Add commands module and classes

---
+++ lib/buildkite_graphql_ruby/commands/branch_status.rb
@@ -0,0 +1,10 @@
+module BuildkiteGraphqlRuby
+  module Commands
+    class BranchStatus
+      def self.run!(options)
+        query = BuildkiteGraphqlRuby::QueryBuilder.new.branch_status(branch: options.branch)
+        query_runner = BuildkiteGraphqlRuby::QueryRunner.new.run_query(query: query, options: options)
+      end
+    end
+  end
+end
---
+++ lib/buildkite_graphql_ruby/commands/command_map.rb
@@ -0,0 +1,15 @@
+module BuildkiteGraphqlRuby
+  module Commands
+    class CommandMap
+      require "buildkite_graphql_ruby/commands/branch_status"
+
+      COMMAND_MAP = {
+        'branch_status' => BranchStatus 
+      }
+
+      def self.get_command(command_string)
+        COMMAND_MAP.fetch(command_string)
+      end
+    end
+  end
+end
